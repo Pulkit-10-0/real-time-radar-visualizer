4c44918485c914403dc7e40473697fc37d1f786a
Pulkit Kapur <pulkitkapur100@gmail.com>
2025-08-27 14:41:14 +0530

    Add files via upload

---
+++ Radar.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+
+Servo myServo;
+const int trigPin = 9;
+const int echoPin = 10;
+
+int angle = 0;
+int step = 1;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(6); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+long getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  return duration * 0.034 / 2; 
+}
+
+void loop() {
+  myServo.write(angle);
+  delay(30);
+
+  long distance = getDistance();
+  if (distance > 200) distance = 200; 
+
+  Serial.print(angle);
+  Serial.print(",");
+  Serial.println(distance);
+
+  angle += step;
+  if (angle >= 180 || angle <= 0) step = -step;
+}
---
+++ Radar.py
@@ -0,0 +1,77 @@
+import pygame
+import math
+import serial
+
+
+ser = serial.Serial('COM5', 9600, timeout=1)  
+
+pygame.init()
+WIDTH, HEIGHT = 800, 800
+CENTER = (WIDTH//2, HEIGHT-50)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Radar Simulation")
+
+clock = pygame.time.Clock()
+
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+DARK_GREEN = (0, 100, 0)
+RED = (255, 0, 0)
+
+
+def draw_grid():
+    screen.fill(BLACK)
+  
+    for r in range(100, 501, 100):
+        pygame.draw.circle(screen, GREEN, CENTER, r, 1)
+   
+    for angle in range(0, 181, 30):
+        rad = math.radians(angle)
+        x = CENTER[0] + 500 * math.cos(rad)
+        y = CENTER[1] - 500 * math.sin(rad)
+        pygame.draw.line(screen, GREEN, CENTER, (x, y), 1)
+
+
+def polar_to_cartesian(angle, dist):
+    rad = math.radians(angle)
+    x = CENTER[0] + dist * math.cos(rad)
+    y = CENTER[1] - dist * math.sin(rad)
+    return int(x), int(y)
+
+running = True
+sweep_angle = 0
+points = []
+
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    draw_grid()
+    if ser.in_waiting > 0:
+        try:
+            line = ser.readline().decode().strip()
+            if line:
+                angle, distance = map(int, line.split(","))
+                if distance < 200:  # valid object
+                    x, y = polar_to_cartesian(angle, distance*2)  
+                    points.append((x, y))
+                    pygame.draw.circle(screen, RED, (x, y), 5)
+        except:
+            pass
+    sweep_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+    pygame.draw.polygon(
+        sweep_surface, (0, 255, 0, 80), 
+        [CENTER, 
+         polar_to_cartesian(sweep_angle-2, 500), 
+         polar_to_cartesian(sweep_angle+2, 500)]
+    )
+    screen.blit(sweep_surface, (0, 0))
+
+
+    sweep_angle = (sweep_angle + 2) % 181
+
+    pygame.display.flip()
+
+pygame.quit()
